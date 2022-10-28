b3f3b075a3c9abd55c7faa4b97dfd911cd58f540
unknown <arjunagnihotrikanpur@gmail.com>
2022-10-28 14:19:50 +0530

    first project

---
+++ app.js
@@ -0,0 +1,30 @@
+// Mobile Hamburger Menu
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+
+// Animations
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        // console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            // To not run the animation everytime, remove this else block
+            entry.target.classList.remove('show');
+        }
+    });
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el)); 
+
+
+
+
